7333abecd52d3021cdde925a790f645a0bcd207f
Pradip Chaudhary <chaudharypradip678@gmail.com>
2022-06-05 16:52:12 +0545

    our team section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
+$(".our-team").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", function () {
     if (window.scrollY > 200) {
