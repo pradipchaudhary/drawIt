a1d307f49d5a2e3d0f950daff4441fc43694defe
Pradip Chaudhary <chaudharypradip678@gmail.com>
2022-06-12 17:05:11 +0545

    Doctor image and menu fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -107,24 +107,6 @@ $(document).ready(function () {
 // function Close() {
 //   $("#dialog").fadeOut();
 // }
-var modal = document.querySelector(".modal");
-var closeButton = document.querySelector(".close-button");
-
-function toggleModal() {
-  modal.classList.toggle("show-modal");
-}
-
-function windowOnClick(event) {
-  if (event.target === modal) {
-    toggleModal();
-  }
-}
-window.onload = function () {
-  toggleModal();
-};
-
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
 
 // === header Fix on scroll
 document.addEventListener("DOMContentLoaded", function () {
