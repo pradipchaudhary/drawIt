cfa3d132567b1d90e1f70e4446dfdceb9f13a0d4
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-12-28 23:36:07 +0545

    header section complete.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import LogoBlack from "./assets/logo-black.png";
+import Header from "./components/Header";
 function App() {
     return (
         <>
             <div>
-                <h1>
-                    <img src={LogoBlack} alt="" width={200} />
-                    drawIt
-                </h1>
+                <Header />
             </div>
         </>
     );
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1 className="brand-name">
+                draw<span>It</span>
+            </h1>
+            <div>Canvas</div>
+            <div>Share</div>
+        </div>
+    );
+};
+
+export default Header;
