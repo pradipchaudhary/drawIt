9630639ba37674060839d35782b5dbac31ca0a4d
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-12-29 22:31:51 +0545

    Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 function App() {
     return (
         <>
             <div>
                 <Header />
+                <Sidebar />
             </div>
         </>
     );
