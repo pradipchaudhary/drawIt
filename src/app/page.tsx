1fb80d91e4d9bd6d9f74f94ed7918592b56081e2
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-01-31 23:45:12 +0545

    convert react to nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Home() {
+    return <main className="flex min-h-screen text-2xl">drawit</main>;
+}
--- src/components/Toggle.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Toggle = () => {
-    return <div id="toggle">Toogle</div>;
-};
-
-export default Toggle;
